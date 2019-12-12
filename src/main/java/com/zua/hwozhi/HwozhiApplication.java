8296a89b61a31bbbccaf34f3aa35609cee6e213c
ning <1747985574@qq.com>
2019-12-12 10:24:35 +0800

    Initial commit

---
+++ src/main/java/com/zua/hwozhi/HwozhiApplication.java
@@ -0,0 +1,13 @@
+package com.zua.hwozhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwozhiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwozhiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zua/hwozhi/HwozhiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zua.hwozhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwozhiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
